659cfffd1ec518f3541b07a79bc6bac18e0cd8b9
Stefan@Lap <stefan.muennich@unibas.ch>
2017-04-18 01:08:44 +0200

    UPDATE (WebernLiveApp3): update to angular 4.0.0 / angular-cli 1.0.0

--- WebernLiveApp3/src/app/shared/shared.module.ts
+++ WebernLiveApp3/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AccordionModule } from 'ng2-bootstrap/accordion';
 
 import { CompileHtmlModule } from './compile-html';
@@ -25,6 +26,7 @@ import { SanitizeHTMLPipe } from './sanitize-html';
     exports: [
         CommonModule,
         FormsModule,
+        BrowserAnimationsModule,
         MaterialModule,
         AccordionModule,
         CompileHtmlModule,
