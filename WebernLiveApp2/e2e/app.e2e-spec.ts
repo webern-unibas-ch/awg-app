291c6891b85ea73beb7690cdca7decaf0f67192f
Stefan@DHLab <stefan.muennich@unibas.ch>
2017-11-16 15:27:32 +0100

    UPDATE (WebernLiveApp2): update to angular cli 1.5.0 && angular 5.0

--- WebernLiveApp2/e2e/app.e2e-spec.ts
+++ WebernLiveApp2/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { WebernLiveApp2Page } from './app.po';
 
 describe('webern-live-app2 App', () => {
-  let page: WebernLiveApp2Page;
+    let page: WebernLiveApp2Page;
 
-  beforeEach(() => {
-    page = new WebernLiveApp2Page();
-  });
+    beforeEach(() => {
+        page = new WebernLiveApp2Page();
+    });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
+    it('should display message saying app works', () => {
+        page.navigateTo();
+        expect(page.getParagraphText()).toEqual('app works!');
+    });
 });
