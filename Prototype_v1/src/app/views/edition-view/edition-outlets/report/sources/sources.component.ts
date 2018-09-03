ed41c4e44f4a1e06538c4d3b8c579f47b23b6cab
Stefan@AWG2 <stefan.muennich@unibas.ch>
2018-09-03 16:50:12 +0200

    FIX (Prototype v1): open source description panel when clicking on corresponding sigle in other panels

--- Prototype_v1/src/app/views/edition-view/edition-outlets/report/sources/sources.component.ts
+++ Prototype_v1/src/app/views/edition-view/edition-outlets/report/sources/sources.component.ts
@@ -12,17 +12,33 @@ export class SourcesComponent implements OnInit {
     @Output() openModalRequest: EventEmitter<any> = new EventEmitter();
     @Output() scrollRequest: EventEmitter<any> = new EventEmitter();
 
+    showDescriptionPanel: boolean = false;
+
     constructor() { }
 
     ngOnInit() {
     }
 
     openModal(identifier: string) {
+        // emit event to open modal
         this.openModalRequest.emit(identifier);
     }
 
     scrollTo(id: string) {
+        // if description is closed: open it before scrolling
+        if (!this.showDescriptionPanel) { this.toggleDescriptionPanel(this.showDescriptionPanel); }
+        // emit scroll request event
         this.scrollRequest.emit(id);
+    }
+
+    toggleDescriptionPanel(showPanel: boolean): boolean {
+        // toggle boolean value of showDescriptionPanel after emitting toggle event
+        return this.showDescriptionPanel = this.togglePanel(showPanel);
+    }
+
+
+    togglePanel(showPanel: boolean): boolean {
+        return showPanel = !showPanel;
     }
 
 }
