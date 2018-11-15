2c684782c7b24d91cb282b92243a508355cbb1a1
Stefan@DHLab <stefan.muennich@unibas.ch>
2018-11-15 11:44:46 +0100

    feat(contact): add OpenStreetMap map to contact page

--- Prototype_v1/src/app/side-info/contact-info/contact-info.component.ts
+++ Prototype_v1/src/app/side-info/contact-info/contact-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
     selector: 'awg-contact-info',
@@ -6,7 +7,23 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./contact-info.component.css']
 })
 export class ContactInfoComponent implements OnInit {
-    constructor() {}
+    private osmRoot = 'https://www.openstreetmap.org/export/embed.html';
+    private osmId =
+        '?bbox=7.582175731658936%2C47.55789611508066%2C7.586840093135835%2C47.56003739001212&amp;layer=mapnik&amp;marker=47.55896585846639%2C7.584506571292877';
+    private osmLinkRoot = 'https://www.openstreetmap.org/';
+    private osmLinkId = '?mlat=47.55897&amp;mlon=7.58451#map=19/47.55897/7.58451';
 
-    ngOnInit() {}
+    unsafeOsmUrl: string;
+    unsafeOsmLinkUrl: string;
+    osmUrl: SafeResourceUrl;
+    osmLinkUrl: SafeResourceUrl;
+
+    constructor(private sanitizer: DomSanitizer) {}
+
+    ngOnInit() {
+        this.unsafeOsmUrl = this.osmRoot + this.osmId;
+        this.unsafeOsmLinkUrl = this.osmLinkRoot + this.osmLinkId;
+        this.osmUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.unsafeOsmUrl);
+        this.osmLinkUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.unsafeOsmLinkUrl);
+    }
 }
