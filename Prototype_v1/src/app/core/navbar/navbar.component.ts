47e2377dd22485d95c379509c29e12dcf1a72d41
Stefan@Lap <stefan.muennich@unibas.ch>
2018-11-17 18:08:10 +0100

    style(core): fix navbar styles

--- Prototype_v1/src/app/core/navbar/navbar.component.ts
+++ Prototype_v1/src/app/core/navbar/navbar.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
     selector: 'awg-navbar',
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-    showMobileNav = false;
+    isCollapsed = true;
+    faEnvelope = faEnvelope;
 
     constructor() {}
 
     ngOnInit() {}
 
     toggleNav(): boolean {
-        return (this.showMobileNav = !this.showMobileNav);
+        return (this.isCollapsed = !this.isCollapsed);
     }
 }
