5a3fe0c0471c7f07b83f544005821dd1e714e811
Stefan@Lap <stefan.muennich@unibas.ch>
2018-11-23 20:41:48 +0100

    fix(core): add separate component for view container

--- Prototype_v1/src/app/core/core.module.ts
+++ Prototype_v1/src/app/core/core.module.ts
@@ -4,11 +4,12 @@ import { SharedModule } from '@awg-shared/shared.module';
 import { FooterModule } from './footer/footer.module';
 
 import { NavbarComponent } from './navbar/navbar.component';
+import { ViewContainerComponent } from './view-container/view-container.component';
 
 @NgModule({
     imports: [SharedModule, FooterModule],
-    declarations: [NavbarComponent],
-    exports: [NavbarComponent, FooterModule]
+    declarations: [NavbarComponent, ViewContainerComponent],
+    exports: [NavbarComponent, ViewContainerComponent, FooterModule]
 })
 export class CoreModule {
     constructor(
