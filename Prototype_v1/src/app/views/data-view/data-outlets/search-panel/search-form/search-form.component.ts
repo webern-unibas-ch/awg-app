dc0436b32843ad92cb3b575e5cb71fb28a702f1c
Stefan@AWG2 <stefan.muennich@unibas.ch>
2018-12-13 18:14:18 +0100

    style(search): change col width for resource detail header

--- Prototype_v1/src/app/views/data-view/data-outlets/search-panel/search-form/search-form.component.ts
+++ Prototype_v1/src/app/views/data-view/data-outlets/search-panel/search-form/search-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { distinctUntilChanged, debounceTime, filter } from 'rxjs/operators';
 
