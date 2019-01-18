c5d3b7012b80450420b6978ad1b5f9c9c820fb5d
Stefan@AWG2 <stefan.muennich@unibas.ch>
2019-01-18 11:45:19 +0100

    fix(app): change LOCALE_ID to "de-DE" globally
    # Why is this change needed?
    Prior to this change, the dates formatted with DatePipe were displayed in English ("en-us").
    
    # How does it address the issue?
    This change registers the resp. LOCALE_ID globally for the whole app.
    
    # Provide links to any relevant tickets, articles or other resources
    - https://stackoverflow.com/questions/34904683/how-to-set-locale-in-datepipe-in-angular-2
    - https://stackoverflow.com/questions/43918376/angular4-locale-for-a-user

--- Prototype_v1/src/app/app.module.ts
+++ Prototype_v1/src/app/app.module.ts
@@ -1,6 +1,8 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localeDeDE from '@angular/common/locales/de';
 
 //
 // main app modules & routes
@@ -10,9 +12,14 @@ import { CoreModule } from '@awg-core/core.module';
 import { SharedModule } from '@awg-shared/shared.module';
 import { SideInfoModule } from '@awg-side-info/side-info.module';
 
+//
+//  load and register the used locale file
+registerLocaleData(localeDeDE);
+
 @NgModule({
     imports: [BrowserModule, HttpClientModule, CoreModule, SharedModule, SideInfoModule, AppRoutingModule],
     declarations: [AppComponent, routedComponents],
+    providers: [{ provide: LOCALE_ID, useValue: 'de-DE' }], // change global LOCALE-ID
     bootstrap: [AppComponent]
 })
 export class AppModule {}
