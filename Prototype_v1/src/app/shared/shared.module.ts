92f3f3edc5b56c54a8243c514def592bdfb2a672
Stefan@Lap <stefan.muennich@unibas.ch>
2019-01-22 00:04:58 +0100

    feat(shared): add json viewer component
    - uses NgxJsonViewerModule
    - input: header string and json data

--- Prototype_v1/src/app/shared/shared.module.ts
+++ Prototype_v1/src/app/shared/shared.module.ts
@@ -21,6 +21,7 @@ import { NgxJsonViewerModule } from 'ngx-json-viewer';
 //
 // shared components
 import { HeadingComponent } from './heading/heading.component';
+import { JsonViewerComponent } from './json-viewer/json-viewer.component';
 import { ModalComponent } from './modal/modal.component';
 import { RouterLinkButtonGroupComponent } from './router-link-button-group/router-link-button-group.component';
 import { TwelveToneSpinnerComponent } from './twelve-tone-spinner/twelve-tone-spinner.component';
@@ -48,6 +49,7 @@ import { OrderByPipe } from './order-by/order-by.pipe';
     ],
     declarations: [
         HeadingComponent,
+        JsonViewerComponent,
         ModalComponent,
         RouterLinkButtonGroupComponent,
         TwelveToneSpinnerComponent,
@@ -70,6 +72,7 @@ import { OrderByPipe } from './order-by/order-by.pipe';
         NgxJsonViewerModule,
 
         HeadingComponent,
+        JsonViewerComponent,
         ModalComponent,
         OrderByPipe,
         RouterLinkButtonGroupComponent,
