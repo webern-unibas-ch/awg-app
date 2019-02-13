d7ce5484193360ba1b082ec44c6f261738d4530d
Stefan@Lap <stefan.muennich@unibas.ch>
2019-02-13 22:22:11 +0100

    fix(search): use ngOnChanges to detect input changes

--- Prototype_v1/src/app/views/data-view/data-outlets/search-panel/search-form/search-form.component.ts
+++ Prototype_v1/src/app/views/data-view/data-outlets/search-panel/search-form/search-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { distinctUntilChanged, debounceTime, filter } from 'rxjs/operators';
@@ -10,7 +10,7 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
     templateUrl: './search-form.component.html',
     styleUrls: ['./search-form.component.css']
 })
-export class SearchFormComponent implements OnInit {
+export class SearchFormComponent implements OnChanges {
     @Input()
     searchValue: string;
     @Output()
@@ -25,10 +25,18 @@ export class SearchFormComponent implements OnInit {
         errorMessage: 'Es wird ein Suchbegriff mit mindestens 3 Zeichen benötigt!'
     };
 
-    constructor(private fb: FormBuilder) {}
-
-    ngOnInit() {
+    constructor(private fb: FormBuilder) {
+        // building form in constructor is necessary
+        // to check for input changes in ngOnChanges
         this.buildForm(this.searchValue);
+    }
+
+    // check for input changes
+    ngOnChanges(changes: SimpleChanges) {
+        if (changes['searchValue']) {
+            // set input value to search form
+            this.searchForm.patchValue({ searchValueControl: this.searchValue });
+        }
     }
 
     // build search form
@@ -37,11 +45,11 @@ export class SearchFormComponent implements OnInit {
             searchValueControl: [searchValue || '', Validators.compose([Validators.required, Validators.minLength(3)])]
         });
 
-        this.onChanges();
+        this.checkForUserInputChange();
     }
 
     // check for changing search values
-    onChanges(): void {
+    checkForUserInputChange(): void {
         this.searchForm
             .get('searchValueControl')
             .valueChanges.pipe(
