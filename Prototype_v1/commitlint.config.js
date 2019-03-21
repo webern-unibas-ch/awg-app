f4c1f869eeac613ebde845ad49a84fb47f112e6e
Stefan@AWG <stefan.muennich@unibas.ch>
2019-03-21 15:51:59 +0100

    build(app): update commitlint schema

--- Prototype_v1/commitlint.config.js
+++ Prototype_v1/commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = { extends: ['@commitlint/config-angular'] };
+module.exports = {
+    extends: ['@commitlint/config-angular'],
+    rules: {
+        'scope-empty': [2, 'never'],
+        'scope-case': [2, 'always', 'lowerCase'],
+        'scope-enum': [2, 'always', ['app', 'home', 'edition', 'search', 'structure', 'contact']]
+    }
+};
