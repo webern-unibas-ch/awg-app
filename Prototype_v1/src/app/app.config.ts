903f2765336682f1e83a597eae242cd7efb3b77a
Stefan@AWG <stefan.muennich@unibas.ch>
2019-03-22 17:12:12 +0100

    fix(app): get global constants from package.json

--- Prototype_v1/src/app/app.config.ts
+++ Prototype_v1/src/app/app.config.ts
@@ -1,10 +1,19 @@
-import { appVersion, appVersionReleaseDate } from '@awg-app/app.version';
+import { appVersion, appVersionReleaseDate, appHomepage } from './app.globals';
 
 export class AppConfig {
     public static get API_ENDPOINT(): string {
         const root = 'http://www.salsah.org/';
         const api = 'api/';
         return root + api;
+    }
+
+    public static get WEBERN_HOME() {
+        const url = 'https://www.anton-webern.ch/';
+        return url;
+    }
+
+    public static get EDITION_HOME(): string {
+        return appHomepage;
     }
 
     public static get VERSION(): string {
--- Prototype_v1/src/app/app.globals.ts
+++ Prototype_v1/src/app/app.globals.ts
@@ -1,4 +1,5 @@
 // THIS IS AN AUTO-GENERATED FILE. DO NOT CHANGE IT MANUALLY!
-// Generated last time on Tue Mar 19 17:45:09 CET 2019
+// Generated last time on Fri Mar 22 17:06:38 CET 2019
 export const appVersion = '0.4.0';
 export const appVersionReleaseDate = '22. Januar 2019';
+export const appHomepage = 'https://edition.anton-webern.ch/app/';
