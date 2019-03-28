9393eed5698053f616d10a7bbb684dfe911335e5
Stefan@AWG <stefan.muennich@unibas.ch>
2019-03-28 11:10:35 +0100

    build(app): update commit message scopes [ci skip]

--- Prototype_v1/commitlint.config.js
+++ Prototype_v1/commitlint.config.js
@@ -2,11 +2,25 @@ module.exports = {
     extends: ['@commitlint/config-angular'],
     rules: {
         'scope-empty': [2, 'never'],
-        'scope-case': [2, 'always', 'lowerCase'],
+        'scope-case': [2, 'always', ['lowerCase', 'upperCase']],
         'scope-enum': [
             2,
             'always',
-            ['app', 'core', 'shared', 'side-info', 'home', 'edition', 'search', 'structure', 'contact']
+            [
+                'app',
+                'core',
+                'shared',
+                'side-info',
+                'home',
+                'edition',
+                'search',
+                'structure',
+                'contact',
+                'README',
+                'CHANGELOG',
+                'LICENSE',
+                'travis'
+            ]
         ]
     }
 };
