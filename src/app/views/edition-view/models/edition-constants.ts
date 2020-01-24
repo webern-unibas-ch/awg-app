74f715678ace35442ac7d92adaff3c40b4e802d5
Stefan@AWG <stefan.muennich@unibas.ch>
2020-01-24 09:43:32 +0100

    feat(edition): add edition constants
    This change introduces EditionConstants to prepare
    easier handling of multiple edition parts and paths.

---
+++ src/app/views/edition-view/models/edition-constants.ts
@@ -0,0 +1,12 @@
+export class EditionConstants {
+    static editionPath = '/edition';
+
+    static op12 = '/op12';
+    static op25 = '/op25';
+
+    static editionIntro = 'intro';
+    static editionDetail = 'detail';
+    static editionReport = 'report';
+
+    static firmNo9Lin28 = 'assets/img/edition/firm_no9_lin28.png';
+}
--- src/app/views/edition-view/models/index.ts
+++ src/app/views/edition-view/models/index.ts
@@ -7,30 +7,33 @@
  *
  */
 
+import { EditionConstants } from './edition-constants';
+
+import { EditionSvgOverlay } from './edition-svg-overlay';
+import { EditionSvgSheet } from './edition-svg-sheet.model';
+
 import { Folio, FolioFormat, FolioContent, FolioSection } from './folio.model';
 import { FolioCalculation, FolioCalculationLine, FolioCalculationPoint } from './folio-calculation.model';
 import { FolioSettings } from './folio-settings.model';
 import { FolioSvgData } from './folio-svg-data.model';
-
-import { EditionSvgSheet } from './edition-svg-sheet.model';
-import { EditionSvgOverlay } from './edition-svg-overlay';
 
 import { Source, SourceList } from './source.model';
 import { Textcritics, TextcriticsList } from './textcritics.model';
 import { ViewBox } from './view-box.model';
 
 export {
+    EditionConstants,
+    EditionSvgOverlay,
+    EditionSvgSheet,
     Folio,
     FolioFormat,
     FolioContent,
     FolioSection,
-    FolioSvgData,
-    EditionSvgSheet,
-    EditionSvgOverlay,
     FolioCalculation,
     FolioCalculationLine,
     FolioCalculationPoint,
     FolioSettings,
+    FolioSvgData,
     Source,
     SourceList,
     Textcritics,
