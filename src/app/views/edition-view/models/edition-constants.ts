f29e27e4e968a97b4c58db45f20d8d9448c14dfa
Stefan@Lap <stefan.muennich@unibas.ch>
2020-01-26 02:31:01 +0100

    docs(edition): add jsdocs to new EditionPath and Constants

--- src/app/views/edition-view/models/edition-constants.ts
+++ src/app/views/edition-view/models/edition-constants.ts
@@ -1,12 +1,42 @@
+/**
+ * The EditionConstants class.
+ *
+ * It is used in the context of the edition view
+ * to store string constants of the editions.
+ */
 export class EditionConstants {
+    /**
+     * The root path to the edition section of the app.
+     */
     static readonly editionPath = '/edition';
 
+    /**
+     * The path to the edition section of a composition: op. 12.
+     */
     static readonly op12 = '/op12';
+
+    /**
+     * The path to the edition section of a composition: op. 25.
+     */
     static readonly op25 = '/op25';
 
+    /**
+     * The path to the intro section of an edition.
+     */
     static readonly editionIntro = 'intro';
+
+    /**
+     * The path to the detail section of an edition.
+     */
     static readonly editionDetail = 'detail';
+
+    /**
+     * The path to the report section of an edition.
+     */
     static readonly editionReport = 'report';
 
+    /**
+     * The path to the assets image of a firm sign: JE No. 9, 28 Lines.
+     */
     static readonly firmJENo9Lin28 = 'assets/img/edition/firm_je_no9_lin28.png';
 }
--- src/app/views/edition-view/models/edition-path.model.ts
+++ src/app/views/edition-view/models/edition-path.model.ts
@@ -1,11 +1,39 @@
 import { EditionConstants } from './edition-constants';
 
+/**
+ * The EditionPath class.
+ *
+ * It is used in the context of the edition view
+ * to store information about the url path of an edition.
+ */
 export class EditionPath {
+    /**
+     * The path to the intro section of an edition.
+     */
     intro: string;
+
+    /**
+     * The path to the detail section of an edition.
+     */
     detail: string;
+
+    /**
+     * The path to the report section of an edition.
+     */
     report: string;
+
+    /**
+     * The root of the path of an edition.
+     */
     root: string;
 
+    /**
+     * Constructor of the EditionPath class.
+     *
+     * It initializes the class with a composition id from the EditionConstants.
+     *
+     * @param {string} compositionId The given id of a composition.
+     */
     constructor(compositionId: string) {
         const delimiter = '/';
 
