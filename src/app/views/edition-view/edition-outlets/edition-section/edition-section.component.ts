84ec7cb8abce1c3a6cdee387616b8174c07e749f
Stefan@Lap <stefan.muennich@unibas.ch>
2020-02-04 22:41:52 +0100

    docs(edition): add docs for new components

--- src/app/views/edition-view/edition-outlets/edition-section/edition-section.component.ts
+++ src/app/views/edition-view/edition-outlets/edition-section/edition-section.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+/**
+ * The EditionSection component.
+ *
+ * It contains the section section
+ * of the edition view of the app.
+ */
 @Component({
     selector: 'awg-edition-section',
     templateUrl: './edition-section.component.html',
     styleUrls: ['./edition-section.component.css']
 })
 export class EditionSectionComponent implements OnInit {
+    /**
+     * Constructor of the EditionSectionComponent.
+     */
     constructor() {}
 
+    /**
+     * Angular life cycle hook: ngOnInit.
+     *
+     * It calls the containing methods
+     * when initializing the component.
+     */
     ngOnInit() {}
 }
