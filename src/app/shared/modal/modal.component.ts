d6b4909428450716f320c568763feb7f19cdf4f9
Stefan@Lap <stefan.muennich@unibas.ch>
2020-02-10 12:02:06 +0100

    fix(edition): adjust modal hint for op25

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -15,6 +15,8 @@ const MODALCONTENTSNIPPETS = {
         'Die edierten Notentexte von <strong>Aa:SkI/1</strong>, <strong>Ab:SkII/1</strong>, <strong>Ac:SkIII/1</strong> und <strong>Ac:SkIII/7</strong> sowie <strong>Ae:SkIV/1</strong> erscheinen im Zusammenhang der vollständigen Edition der <i>Vier Lieder</i> op. 12 in AWG I/5.',
     op12_editionComingSoon:
         '<p>Die Einleitungen, edierten Notentexte und Kritischen Berichte zu</p><ul class="none"><li>Werkedition der Druckfassung der <i>Vier Lieder</i> op. 12 <br/> Textedition von Nr. I „<i>Der Tag ist vergangen</i>“ (Fassung 1) <br/> Textedition von Nr. I „<i>Der Tag ist vergangen</i>“ (Fassung 2) <br/> Textedition von Nr. IV <i>Gleich und Gleich</i> (Fassung 1) </li></ul><p> erscheinen im Zusammenhang der vollständigen Edition der <i>Vier Lieder</i> op. 12 in AWG I/5.</p>',
+    op25_sheetComingSoon:
+        'Die edierten Notentexte weiterer Skizzen der <i>Drei Lieder nach Gedichten von Hildegard Jone</i> op. 25 erscheinen in Kürze (02/2020).',
     op25_sourceNotA:
         '<p>Die Beschreibung der Quellen <strong>B</strong> sowie <strong>D–E</strong> einschließlich der darin gegebenenfalls enthaltenen Korrekturen erfolgt im Zusammenhang der vollständigen Edition der <i>Drei Lieder nach Gedichten von Hildegard Jone</i> op. 25 in AWG I/5.</p>',
     M198:
