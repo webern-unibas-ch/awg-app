7a1cd296a4234e776510fa902872c48f5dc1fa45
Stefan@AWG <stefan.muennich@unibas.ch>
2020-07-16 12:01:44 +0200

    chore(release): 0.7.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,8 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### [0.7.4](https://github.com/webern-unibas-ch/awg-app/compare/v0.7.3...v0.7.4) (2020-07-16)
+
 ### [0.7.3](https://github.com/webern-unibas-ch/awg-app/compare/v0.7.2...v0.7.3) (2020-04-09)
 
 ### Bug Fixes
--- src/app/app.globals.ts
+++ src/app/app.globals.ts
@@ -1,15 +1,15 @@
 // THIS IS AN AUTO-GENERATED FILE. DO NOT CHANGE IT MANUALLY!
-// Generated last time on Thu Apr  9 12:48:35     2020
+// Generated last time on Thu Jul 16 12:01:44 CEST 2020
 
 /**
  * The latest version of the AWG App
  */
-export const appVersion = '0.7.3';
+export const appVersion = '0.7.4';
 
 /**
  * The release date of the latest version of the AWG App
  */
-export const appVersionReleaseDate = '09. April 2020';
+export const appVersionReleaseDate = '16. Juli 2020';
 
 /**
  * The URL of the AWG App
