e6e02d2e84a0b5677ffc8ea5f19937ababd68bea
Stefan@Lap <stefan.muennich@unibas.ch>
2021-06-26 18:49:40 +0200

    build(app): clean up framework after update to angular 12

--- src/test.ts
+++ src/test.ts
@@ -4,7 +4,7 @@
 // For custom matchers, see https://stackoverflow.com/a/44996479
 import '@testing/custom-matchers';
 
-import 'zone.js/dist/zone-testing';
+import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
