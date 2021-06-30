fd1da1ee1bb7ffcfbea1e6b6239c8f6237b4b367
Stefan@Lap <stefan.muennich@unibas.ch>
2021-06-30 23:10:10 +0200

    build(deps-dev): bump @angular-eslint packages from 4.3.0 to 12.2.0

--- commitlint.config.js
+++ commitlint.config.js
@@ -9,6 +9,7 @@ module.exports = {
             [
                 'app',
                 'deps',
+                'deps-dev',
                 'core',
                 'shared',
                 'side-info',
