ad5d91a672b53300338b9cbe32bb6ca64618f33c
Stefan@Lap <stefan.muennich@unibas.ch>
2022-01-15 13:29:15 +0100

    chore(release): 0.7.19

--- src/app/app.globals.ts
+++ src/app/app.globals.ts
@@ -1,15 +1,15 @@
 // THIS IS AN AUTO-GENERATED FILE. DO NOT CHANGE IT MANUALLY!
-// Generated last time on Fri Dec 17 02:17:14     2021
+// Generated last time on Sat Jan 15 13:29:14     2022
 
 /**
  * The latest version of the AWG App
  */
-export const appVersion = '0.7.18';
+export const appVersion = '0.7.19';
 
 /**
  * The release date of the latest version of the AWG App
  */
-export const appVersionReleaseDate = '17. Dezember 2021';
+export const appVersionReleaseDate = '15. Januar 2022';
 
 /**
  * The URL of the AWG App
