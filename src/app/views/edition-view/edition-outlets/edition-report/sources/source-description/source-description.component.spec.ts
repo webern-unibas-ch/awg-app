599e3da377c28ade29db24d5c0acdb1b64333938
Stefan@Lap <stefan.muennich@unibas.ch>
2022-02-10 22:32:57 +0100

    test(edition): adjust tests for SourceDescriptionComponent

--- src/app/views/edition-view/edition-outlets/edition-report/sources/source-description/source-description.component.spec.ts
+++ src/app/views/edition-view/edition-outlets/edition-report/sources/source-description/source-description.component.spec.ts
@@ -67,7 +67,7 @@ describe('SourceDescriptionComponent (DONE)', () => {
                 {
                     id: 'sourceAa',
                     siglum: 'Aa',
-                    location: '',
+                    location: 'Wien, Testcentre.',
                     description: [
                         '<img class="img-thumbnail" [src]="ref.FIRM_SIGNS.OP12.A[0].route" [title]="ref.FIRM_SIGNS.OP12.A[0].full" [alt]="ref.FIRM_SIGNS.OP12.A[0].short" /> <br /> auf Bl. 1<sup>r</sup> unten links (Bl. 1); <br />Notenpapier, 16 Systeme, Format: quer 175 × 270 mm, kein Firmenzeichen (Bl. 2).',
                         '<span class="caps">Inhalt</span>: (<a (click)="ref.openModal(\'OP12_SHEET_COMING_SOON\')"><strong>SkI/1a</strong></a>). (<a (click)="ref.selectSvgSheet(\'Aa:SkI/2\')"><strong>SkI/2</strong></a>).',
@@ -93,13 +93,12 @@ describe('SourceDescriptionComponent (DONE)', () => {
 
     describe('BEFORE initial data binding', () => {
         it('should not have sourceDescriptionListData', () => {
-            expect(component.sourceDescriptionListData).toBeUndefined('should be undefined');
+            expect(component.sourceDescriptionListData).withContext('should be undefined').toBeUndefined();
         });
 
         it('should have ref', () => {
             expect(component.ref).toBeTruthy();
-            // @ts-ignore
-            expect(component.ref).toEqual(component, `should equal ${component}`);
+            expect(component.ref).withContext(`should equal ${component}`).toEqual(component);
         });
 
         describe('VIEW', () => {
@@ -118,83 +117,173 @@ describe('SourceDescriptionComponent (DONE)', () => {
             fixture.detectChanges();
         });
 
+        it('should have sourceDescriptionListData', () => {
+            expect(component.sourceDescriptionListData).toBeTruthy();
+            expect(component.sourceDescriptionListData)
+                .withContext(`should equal ${expectedSourceDescriptionListData}`)
+                .toEqual(expectedSourceDescriptionListData);
+        });
+
         describe('VIEW', () => {
-            it('... should contain one description list div', () => {
+            it('... should contain one main description list div', () => {
                 getAndExpectDebugElementByCss(compDe, 'div.awg-source-description-list', 1, 1);
             });
 
-            it('... should contain two description divs in main div', () => {
+            it('... should contain as many description divs in main list div as description data has source entries', () => {
                 getAndExpectDebugElementByCss(
                     compDe,
                     'div.awg-source-description-list > div.awg-source-description',
-                    2,
-                    2
+                    expectedSourceDescriptionListData.sources.length,
+                    expectedSourceDescriptionListData.sources.length
                 );
             });
 
-            it('... should contain two paragraphs in first description div', () => {
-                const divDes = getAndExpectDebugElementByCss(
-                    compDe,
-                    'div.awg-source-description-list > div.awg-source-description',
-                    2,
-                    2
-                );
+            describe('... first description div', () => {
+                it('... should contain two paragraphs', () => {
+                    const divDes = getAndExpectDebugElementByCss(
+                        compDe,
+                        'div.awg-source-description-list > div.awg-source-description',
+                        2,
+                        2
+                    );
 
-                const pDes = getAndExpectDebugElementByCss(divDes[0], 'p', 2, 2);
+                    getAndExpectDebugElementByCss(divDes[0], 'p', 2, 2);
+                });
 
-                const pCmp0 = pDes[0].nativeElement;
-                const pCmp1 = pDes[1].nativeElement;
+                it('... the first one displaying siglum', () => {
+                    const divDes = getAndExpectDebugElementByCss(
+                        compDe,
+                        'div.awg-source-description-list > div.awg-source-description',
+                        2,
+                        2
+                    );
 
-                expect(pCmp0.textContent).toBeDefined('should be defined');
-                expect(pCmp0.textContent).toBe(
-                    expectedSourceDescriptionListData.sources[0].siglum,
-                    `should be ${expectedSourceDescriptionListData.sources[0].siglum}`
-                );
+                    const pDes = getAndExpectDebugElementByCss(divDes[0], 'p', 2, 2);
 
-                expect(pCmp1.textContent).toBeDefined('should be defined');
-                expect(pCmp1.textContent).toBe(
-                    expectedSourceDescriptionListData.sources[0].location,
-                    `should be ${expectedSourceDescriptionListData.sources[0].location}`
-                );
+                    const pCmp = pDes[0].nativeElement;
+
+                    expect(pCmp).toHaveClass('awg-source-description-siglum');
+                    expect(pCmp.textContent).withContext('should be defined').toBeDefined();
+                    expect(pCmp.textContent.trim())
+                        .withContext(`should be ${expectedSourceDescriptionListData.sources[0].siglum.trim()}`)
+                        .toBe(expectedSourceDescriptionListData.sources[0].siglum.trim());
+                });
+
+                it('... the second one displaying location', () => {
+                    const divDes = getAndExpectDebugElementByCss(
+                        compDe,
+                        'div.awg-source-description-list > div.awg-source-description',
+                        2,
+                        2
+                    );
+
+                    const pDes = getAndExpectDebugElementByCss(divDes[0], 'p', 2, 2);
+
+                    const pCmp = pDes[1].nativeElement;
+
+                    expect(pCmp).toHaveClass('awg-source-description-location');
+                    expect(pCmp.textContent).withContext('should be defined').toBeDefined();
+                    expect(pCmp.textContent.trim())
+                        .withContext(`should be ${expectedSourceDescriptionListData.sources[0].location.trim()}`)
+                        .toBe(expectedSourceDescriptionListData.sources[0].location.trim());
+                });
             });
 
-            it('... should contain 3 paragraphs in second description div', () => {
-                const divDes = getAndExpectDebugElementByCss(
-                    compDe,
-                    'div.awg-source-description-list > div.awg-source-description',
-                    2,
-                    2
-                );
+            describe('... second description div', () => {
+                it('... should contain 4 paragraphs', () => {
+                    const divDes = getAndExpectDebugElementByCss(
+                        compDe,
+                        'div.awg-source-description-list > div.awg-source-description',
+                        2,
+                        2
+                    );
 
-                const pDes = getAndExpectDebugElementByCss(divDes[1], 'p', 3, 3);
+                    getAndExpectDebugElementByCss(divDes[1], 'p', 4, 4);
+                });
 
-                const pCmp0 = pDes[0].nativeElement;
-                const pCmp1 = pDes[1].nativeElement;
-                const pCmp2 = pDes[2].nativeElement;
+                it('... the first one displaying siglum', () => {
+                    const divDes = getAndExpectDebugElementByCss(
+                        compDe,
+                        'div.awg-source-description-list > div.awg-source-description',
+                        2,
+                        2
+                    );
 
-                expect(pCmp0.textContent).toBeDefined('should be defined');
-                expect(pCmp0.textContent).toBe(
-                    expectedSourceDescriptionListData.sources[1].siglum,
-                    `should be ${expectedSourceDescriptionListData.sources[1].siglum}`
-                );
+                    const pDes = getAndExpectDebugElementByCss(divDes[1], 'p', 4, 4);
 
-                // Process HTML expression of description
-                const htmlDescription = mockDocument.createElement('p');
-                htmlDescription.innerHTML = expectedSourceDescriptionListData.sources[1].description[0];
+                    const pCmp = pDes[0].nativeElement;
 
-                expect(pCmp1.textContent).toBeDefined('should be defined');
-                expect(pCmp1.textContent.trim()).toBe(
-                    htmlDescription.textContent.trim(),
-                    `should be ${htmlDescription.textContent.trim()}`
-                );
+                    expect(pCmp).toHaveClass('awg-source-description-siglum');
+                    expect(pCmp.textContent).withContext('should be defined').toBeDefined();
+                    expect(pCmp.textContent.trim())
+                        .withContext(`should be ${expectedSourceDescriptionListData.sources[1].siglum.trim()}`)
+                        .toBe(expectedSourceDescriptionListData.sources[1].siglum.trim());
+                });
 
-                htmlDescription.innerHTML = expectedSourceDescriptionListData.sources[1].description[1];
+                it('... the second one displaying location', () => {
+                    const divDes = getAndExpectDebugElementByCss(
+                        compDe,
+                        'div.awg-source-description-list > div.awg-source-description',
+                        2,
+                        2
+                    );
 
-                expect(pCmp2.textContent).toBeDefined('should be defined');
-                expect(pCmp2.textContent.trim()).toBe(
-                    htmlDescription.textContent.trim(),
-                    `should be ${htmlDescription.textContent.trim()}`
-                );
+                    const pDes = getAndExpectDebugElementByCss(divDes[1], 'p', 4, 4);
+
+                    const pCmp = pDes[1].nativeElement;
+
+                    expect(pCmp).toHaveClass('awg-source-description-location');
+                    expect(pCmp.textContent).withContext('should be defined').toBeDefined();
+                    expect(pCmp.textContent.trim())
+                        .withContext(`should be ${expectedSourceDescriptionListData.sources[1].location.trim()}`)
+                        .toBe(expectedSourceDescriptionListData.sources[1].location.trim());
+                });
+
+                it('... the third one displaying the first description', () => {
+                    const divDes = getAndExpectDebugElementByCss(
+                        compDe,
+                        'div.awg-source-description-list > div.awg-source-description',
+                        2,
+                        2
+                    );
+
+                    const pDes = getAndExpectDebugElementByCss(divDes[1], 'p', 4, 4);
+
+                    const pCmp = pDes[2].nativeElement;
+
+                    // Process HTML expression of description entry
+                    const htmlDescriptionEntry = mockDocument.createElement('p');
+                    htmlDescriptionEntry.innerHTML = expectedSourceDescriptionListData.sources[1].description[0];
+
+                    expect(pCmp).toHaveClass('awg-source-description-entry');
+                    expect(pCmp.textContent).withContext('should be defined').toBeDefined();
+                    expect(pCmp.textContent.trim())
+                        .withContext(`should be ${htmlDescriptionEntry.textContent.trim()}`)
+                        .toBe(htmlDescriptionEntry.textContent.trim());
+                });
+
+                it('... the foruth one displaying the second description', () => {
+                    const divDes = getAndExpectDebugElementByCss(
+                        compDe,
+                        'div.awg-source-description-list > div.awg-source-description',
+                        2,
+                        2
+                    );
+
+                    const pDes = getAndExpectDebugElementByCss(divDes[1], 'p', 4, 4);
+
+                    const pCmp = pDes[3].nativeElement;
+
+                    // Process HTML expression of description entry
+                    const htmlDescriptionEntry = mockDocument.createElement('p');
+                    htmlDescriptionEntry.innerHTML = expectedSourceDescriptionListData.sources[1].description[1];
+
+                    expect(pCmp).toHaveClass('awg-source-description-entry');
+                    expect(pCmp.textContent).withContext('should be defined').toBeDefined();
+                    expect(pCmp.textContent.trim())
+                        .withContext(`should be ${htmlDescriptionEntry.textContent.trim()}`)
+                        .toBe(htmlDescriptionEntry.textContent.trim());
+                });
             });
         });
 
@@ -206,10 +295,11 @@ describe('SourceDescriptionComponent (DONE)', () => {
                     2,
                     2
                 );
-                const pDes = getAndExpectDebugElementByCss(divDes[1], 'p', 3, 3);
+                // Find description paragraphs
+                const pDes = getAndExpectDebugElementByCss(divDes[1], 'p.awg-source-description-entry', 2, 2);
 
-                // Find anchors in third paragraph
-                const anchorDes = getAndExpectDebugElementByCss(pDes[2], 'a', 2, 2);
+                // Find anchors in second description paragraph
+                const anchorDes = getAndExpectDebugElementByCss(pDes[1], 'a', 2, 2);
 
                 // Click on first anchor with modal call
                 clickAndAwaitChanges(anchorDes[0], fixture);
@@ -217,10 +307,23 @@ describe('SourceDescriptionComponent (DONE)', () => {
                 expectSpyCall(openModalSpy, 1, expectedModalSnippet);
             }));
 
-            it('... should not emit anything if no id is provided', () => {
-                component.openModal(undefined);
+            describe('... should not emit anything if ', () => {
+                it('... id is undefined', () => {
+                    component.openModal(undefined);
 
-                expectSpyCall(openModalRequestEmitSpy, 0, undefined);
+                    expectSpyCall(openModalRequestEmitSpy, 0);
+                });
+
+                it('... id is null', () => {
+                    component.openModal(undefined);
+
+                    expectSpyCall(openModalRequestEmitSpy, 0, null);
+                });
+                it('... id is empty string', () => {
+                    component.openModal('');
+
+                    expectSpyCall(openModalRequestEmitSpy, 0);
+                });
             });
 
             it('... should emit id of given modal snippet', () => {
@@ -238,10 +341,11 @@ describe('SourceDescriptionComponent (DONE)', () => {
                     2,
                     2
                 );
-                const pDes = getAndExpectDebugElementByCss(divDes[1], 'p', 3, 3);
+                // Find description paragraphs
+                const pDes = getAndExpectDebugElementByCss(divDes[1], 'p.awg-source-description-entry', 2, 2);
 
-                // Find anchors in third paragraph
-                const anchorDes = getAndExpectDebugElementByCss(pDes[2], 'a', 2, 2);
+                // Find anchors in second paragraph
+                const anchorDes = getAndExpectDebugElementByCss(pDes[1], 'a', 2, 2);
 
                 // CLick on second anchor (with selectSvgSheet call)
                 clickAndAwaitChanges(anchorDes[1], fixture);
@@ -249,10 +353,22 @@ describe('SourceDescriptionComponent (DONE)', () => {
                 expectSpyCall(selectSvgSheetSpy, 1, expectedSvgSheet.id);
             }));
 
-            it('... should not emit anything if no id is provided', () => {
-                component.selectSvgSheet(undefined);
+            describe('... should not emit anything if ', () => {
+                it('... id is undefined', () => {
+                    component.selectSvgSheet(undefined);
 
-                expectSpyCall(selectSvgSheetRequestEmitSpy, 0, undefined);
+                    expectSpyCall(selectSvgSheetRequestEmitSpy, 0);
+                });
+                it('... id is null', () => {
+                    component.selectSvgSheet(null);
+
+                    expectSpyCall(selectSvgSheetRequestEmitSpy, 0);
+                });
+                it('... id is empty string', () => {
+                    component.selectSvgSheet('');
+
+                    expectSpyCall(selectSvgSheetRequestEmitSpy, 0);
+                });
             });
 
             it('... should emit id of selected svg sheet', () => {
