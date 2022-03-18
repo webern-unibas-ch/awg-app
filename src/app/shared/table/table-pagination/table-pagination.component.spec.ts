3992c4f84bc32ccee82c484c589bb930a0dc1ff7
musicEnfanthen <stefan.muennich@unibas.ch>
2022-03-18 01:32:48 +0100

    fix(shared): move logic for table pagination in separate component

---
+++ src/app/shared/table/table-pagination/table-pagination.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TablePaginationComponent } from './table-pagination.component';
+
+describe('TablePaginationComponent', () => {
+    let component: TablePaginationComponent;
+    let fixture: ComponentFixture<TablePaginationComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [TablePaginationComponent],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TablePaginationComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+});
