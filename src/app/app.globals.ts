1cf00f2d64001ddad86cf8f6b35653e1fd246b16
musicEnfanthen <stefan.muennich@unibas.ch>
2022-03-23 13:00:05 +0100

    chore(release): 0.8.1

--- src/app/app.globals.ts
+++ src/app/app.globals.ts
@@ -1,15 +1,15 @@
 // THIS IS AN AUTO-GENERATED FILE. DO NOT CHANGE IT MANUALLY!
-// Generated last time on Sat Feb 26 08:50:29     2022
+// Generated last time on Wed Mar 23 13:00:04     2022
 
 /**
  * The latest version of the AWG App
  */
-export const appVersion = '0.8.0';
+export const appVersion = '0.8.1';
 
 /**
  * The release date of the latest version of the AWG App
  */
-export const appVersionReleaseDate = '26. Februar 2022';
+export const appVersionReleaseDate = '23. März 2022';
 
 /**
  * The URL of the AWG App
