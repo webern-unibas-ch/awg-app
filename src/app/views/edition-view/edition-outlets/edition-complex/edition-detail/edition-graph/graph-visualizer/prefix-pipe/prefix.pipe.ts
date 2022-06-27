51cbb5a7e60b1ade86354bb5f195e69cd790f81b
musicEnfanthen <stefan.muennich@unibas.ch>
2022-06-28 00:57:58 +0200

    fix(edition): add some more default prefixes to PrefixPipe

--- src/app/views/edition-view/edition-outlets/edition-complex/edition-detail/edition-graph/graph-visualizer/prefix-pipe/prefix.pipe.ts
+++ src/app/views/edition-view/edition-outlets/edition-complex/edition-detail/edition-graph/graph-visualizer/prefix-pipe/prefix.pipe.ts
@@ -16,11 +16,18 @@ export class PrefixPipe implements PipeTransform {
      */
     prefixes: Prefix[] = [
         new Prefix('awg', 'https://edition.anton-webern.ch/webern-onto#'),
+        new Prefix('dbo', 'http://dbpedia.org/ontology/'),
+        new Prefix('dbp', 'http://dbpedia.org/property/'),
+        new Prefix('dbpedia', 'http://dbpedia.org/resource/'),
         new Prefix('dc', 'http://purl.org/dc/elements/1.1/'),
+        new Prefix('dcterms', 'http://purl.org/dc/terms/'),
+        new Prefix('foaf', 'http://xmlns.com/foaf/0.1/'),
+        new Prefix('mo', 'http://purl.org/ontology/mo/'),
         new Prefix('owl', 'http://www.w3.org/2002/07/owl#'),
         new Prefix('prov', 'http://www.w3.org/ns/prov#'),
         new Prefix('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
         new Prefix('rdfs', 'http://www.w3.org/2000/01/rdf-schema#'),
+        new Prefix('skos', 'http://www.w3.org/2004/02/skos/core#'),
         new Prefix('xsd', 'http://www.w3.org/2001/XMLSchema#'),
     ];
 
