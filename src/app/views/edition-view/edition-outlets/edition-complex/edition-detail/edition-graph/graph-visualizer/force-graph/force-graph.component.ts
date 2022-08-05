b2f2ee3fa0339638df8aee905ad39f7213ac4165
musicEnfanthen <stefan.muennich@unibas.ch>
2022-08-05 19:06:53 +0200

    feat(shared): create SliderConfig class

--- src/app/views/edition-view/edition-outlets/edition-complex/edition-detail/edition-graph/graph-visualizer/force-graph/force-graph.component.ts
+++ src/app/views/edition-view/edition-outlets/edition-complex/edition-detail/edition-graph/graph-visualizer/force-graph/force-graph.component.ts
@@ -22,6 +22,7 @@ import { debounceTime, takeUntil } from 'rxjs/operators';
 
 import { faCompressArrowsAlt } from '@fortawesome/free-solid-svg-icons';
 
+import { SliderConfig } from '@awg-app/shared/shared-models/slider-config';
 import {
     D3DragBehaviour,
     D3Selection,
@@ -138,13 +139,7 @@ export class ForceGraphComponent implements OnInit, OnChanges, OnDestroy {
      *
      * It keeps the default values for the zoom slider input.
      */
-    sliderConfig = {
-        initial: 1,
-        min: 0.1,
-        max: 3,
-        step: 1 / 100,
-        value: 1,
-    };
+    sliderConfig = new SliderConfig(1, 0.1, 3, 1 / 100, 1);
 
     /**
      * Private variable: _svg.
@@ -839,7 +834,7 @@ export class ForceGraphComponent implements OnInit, OnChanges, OnDestroy {
      * @returns {number} The rounded value.
      */
     private _roundToNearestScaleStep(value: number): number {
-        const steps = this.sliderConfig.step;
+        const steps = this.sliderConfig.stepSize;
 
         // Count decimals of a given value
         // Cf. https://stackoverflow.com/a/17369245
