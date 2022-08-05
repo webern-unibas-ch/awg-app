0f0e94a8eda440956d92a22ec93abb829037a2c8
musicEnfanthen <stefan.muennich@unibas.ch>
2022-08-05 21:23:06 +0200

    docs(edition): add docs for EditionSvgDrawingService

--- src/app/views/edition-view/services/edition-svg-drawing.service.ts
+++ src/app/views/edition-view/services/edition-svg-drawing.service.ts
@@ -5,25 +5,71 @@ import { D3Selection, ViewBox } from '@awg-views/edition-view/models';
 import * as d3_selection from 'd3-selection';
 import * as d3_fetch from 'd3-fetch';
 
+/**
+ * The EditionSvgDrawing service.
+ *
+ * It handles the creation and handling with SVGs via the D3 library.
+ *
+ * Provided in: `root`.
+ */
 @Injectable({ providedIn: 'root' })
 export class EditionSvgDrawingService {
+    /**
+     * Public variable: deselectionFillColor.
+     *
+     * It keeps the fill color for deselected overlays.
+     */
     deselectionFillColor = 'orange';
+
+    /**
+     * Public variable: selectionFillColor.
+     *
+     * It keeps the fill color for selected overlays.
+     */
     selectionFillColor = 'green';
 
-    // Magic numbers for overlay boxes
+    /**
+     * Private variable: _overlayBoxesOpacity.
+     *
+     * It keeps the default opacity for an overlay box.
+     */
     private _overlayBoxesOpacity = 0.3;
+
+    /**
+     * Private variable: _overlayBoxesOpacity.
+     *
+     * It keeps a magic number for (optional) additional space of an overlay box.
+     */
     private _overlayBoxAdditionalSpace = 5.5;
+
+    /**
+     * Private variable: _overlayBoxesOpacity.
+     *
+     * It keeps a magic number for (optional) corner radius of an overlay box.
+     */
     private _overlayBoxCornerRadius = 2;
 
-    async createSvg(svgFilePath: string, svgEl: SVGSVGElement, svgRootEl: SVGGElement): Promise<D3Selection> {
+    /**
+     * Public async method: createSvg.
+     *
+     * It creates an D3 selection representation of an svg file (given by its path)
+     * and returns a promise of the selection.
+     *
+     * @param {string} svgFilePath The given path to the svg file.
+     * @param {SVGSVGElement} svgEl The given SVGSVGElement.
+     * @param {SVGGElement} svgRootGroupEl The given SVGGElement.
+     *
+     * @returns {Promise<D3Selection>} A promise that resolves to the D3 selection.
+     */
+    async createSvg(svgFilePath: string, svgEl: SVGSVGElement, svgRootGroupEl: SVGGElement): Promise<D3Selection> {
         if (!svgFilePath) {
             throw new Error('svgFilePath is not defined');
         }
         if (!svgEl) {
             throw new Error('svgEl is not defined');
         }
-        if (!svgRootEl) {
-            throw new Error('svgRootEl is not defined');
+        if (!svgRootGroupEl) {
+            throw new Error('svgRootGroupEl is not defined');
         }
 
         // Fetch the SVG file
@@ -36,11 +82,11 @@ export class EditionSvgDrawingService {
 
         // Get the reference to the svg root element in the HTML template and append the childNodes of the SVG XML DOM tree to it
         while (fetchedSvgXML.firstChild) {
-            svgRootEl.appendChild(fetchedSvgXML.firstChild);
+            svgRootGroupEl.appendChild(fetchedSvgXML.firstChild);
         }
 
         // Get the reference to the svg element in the HTML template and append the svg root element to it
-        svgEl.appendChild(svgRootEl);
+        svgEl.appendChild(svgRootGroupEl);
 
         const svg = d3_selection
             .select(svgEl)
@@ -55,13 +101,27 @@ export class EditionSvgDrawingService {
         return svg;
     }
 
-    createSVGOverlayGroup(svgRoot: D3Selection | undefined, id: string, dim: DOMRect, type: string): D3Selection {
-        if (!svgRoot || !id) {
+    /**
+     * Public method: createSVGOverlayGroup.
+     *
+     * It creates a D3 selection representation of an overlay group (rect)
+     * with a given type for an element identified by the given id in the svgRootGroup
+     * and returns that selection.
+     *
+     * @param {D3Selection} svgRootGroup The given D3 selection of the SVG root group.
+     * @param {string} id The given id.
+     * @param {DOMRect} dim The given dimensions of the SVG element.
+     * @param {string} type The given type.
+     *
+     * @returns {D3Selection} The selection of the overlay group.
+     */
+    createSVGOverlayGroup(svgRootGroup: D3Selection | undefined, id: string, dim: DOMRect, type: string): D3Selection {
+        if (!svgRootGroup || !id) {
             return undefined;
         }
 
         // Get D3 selection of target group
-        const targetGroupSelection: D3Selection = this.getD3SelectionById(svgRoot, id);
+        const targetGroupSelection: D3Selection = this.getD3SelectionById(svgRootGroup, id);
 
         // Append overlay group to target group
         targetGroupSelection.append('g').attr('class', `${type}-overlay-group`);
@@ -82,20 +142,39 @@ export class EditionSvgDrawingService {
             .attr('class', `${type}-overlay-group-box`);
     }
 
-    fillD3SelectionWithColor(svgSel: D3Selection | undefined, color: string): void {
-        if (!svgSel) {
+    /**
+     * Public method: fillD3SelectionWithColor.
+     *
+     * It fills the given D3 selection with the given color.
+     *
+     * @param {D3Selection} svgEl The given D3 selection.
+     * @param {string} color The given color.
+     *
+     * @returns {void} Fills the selection.
+     */
+    fillD3SelectionWithColor(svgEl: D3Selection | undefined, color: string): void {
+        if (!svgEl) {
             return;
         }
-        svgSel.attr('fill', color);
+        svgEl.attr('fill', color);
     }
 
-    getContainerDimensions(container: ElementRef, width: number, height: number): { width: number; height: number } {
-        if (!container) {
+    /**
+     * Public method: getContainerDimensions.
+     *
+     * It returns the dimensions (width and height) of a given container element.
+     *
+     * @param {ElementRef} containerEl The given container element.
+     *
+     * @returns {{ width: number; height: number }} The dimensions (width and height) of the container element.
+     */
+    getContainerDimensions(containerEl: ElementRef): { width: number; height: number } {
+        if (!containerEl) {
             return { width: undefined, height: undefined };
         }
 
-        const w = width ? width : this._getElementDimensions(container).width;
-        const h = height ? height : this._getElementDimensions(container).height;
+        const w = containerEl.nativeElement.clientWidth;
+        const h = containerEl.nativeElement.clientHeight;
 
         return {
             width: w,
@@ -103,15 +182,34 @@ export class EditionSvgDrawingService {
         };
     }
 
-    getD3SelectionById(svgRoot: D3Selection, id: string): D3Selection {
-        if (!svgRoot) {
+    /**
+     * Public method: getD3SelectionById.
+     *
+     * It selects an element with the given id, if available, from the given svgRootGroup.
+     *
+     * @param {D3Selection} svgRootGroup The given D3 selection of the SVG root group.
+     * @param {string} id The given id.
+     *
+     * @returns {D3Selection} The D3 selection of the found element.
+     */
+    getD3SelectionById(svgRootGroup: D3Selection, id: string): D3Selection {
+        if (!svgRootGroup) {
             return undefined;
         }
-        return svgRoot.select('#' + id);
+        return svgRootGroup.select('#' + id);
     }
 
-    getLinkBoxes(svgRoot: D3Selection): void {
-        const linkBoxGroups = svgRoot.selectAll('g.link-box');
+    /**
+     * Public method: getLinkBoxes.
+     *
+     * It creates and selects all elements with the "link-box" class, if available, from the given svgRootGroup.
+     *
+     * @param {D3Selection} svgRootGroup The given D3 selection of the SVG root group.
+     *
+     * @returns {void} Creates and selects the elements.
+     */
+    getLinkBoxes(svgRootGroup: D3Selection): void {
+        const linkBoxGroups = svgRootGroup.selectAll('g.link-box');
 
         const _self = this;
 
@@ -152,43 +250,65 @@ export class EditionSvgDrawingService {
         });
     }
 
-    getOverlayGroupRectSelection(svgRoot: D3Selection, id: string, type: string): D3Selection {
-        if (!svgRoot || !id) {
+    /**
+     * Public method: getOverlayGroupRectSelection.
+     *
+     * It selects an overlay group box (rect) with a given type from an element identified by the given id in the given svgRootGroup.
+     *
+     * @param {D3Selection} svgRootGroup The given D3 selection of the SVG root group.
+     * @param {string} id The given id.
+     * @param {string} type The given type.
+     *
+     * @returns {D3Selection} The D3 selection of the found element.
+     */
+    getOverlayGroupRectSelection(svgRootGroup: D3Selection, id: string, type: string): D3Selection {
+        if (!svgRootGroup || !id) {
             return undefined;
         }
         // Get D3 selection of target group
-        const targetGroupSelection: D3Selection = this.getD3SelectionById(svgRoot, id);
+        const targetGroupSelection: D3Selection = this.getD3SelectionById(svgRootGroup, id);
         // Get D3 selection of overlay group box
         return targetGroupSelection.select(`rect.${type}-overlay-group-box`);
     }
 
-    getTkkGroups(svgRoot: D3Selection): D3Selection {
-        if (!svgRoot) {
+    /**
+     * Public method: getTkkGroups.
+     *
+     * It selects all group elements with the "tkk" class, if available, from the given svgRootGroup.
+     *
+     * @param {D3Selection} svgRootGroup The given D3 selection of the SVG root group.
+     *
+     * @returns {D3Selection} The D3 selection of the found element.
+     */
+    getTkkGroups(svgRootGroup: D3Selection): D3Selection {
+        if (!svgRootGroup) {
             return undefined;
         }
-        return svgRoot.selectAll('g.tkk');
+        return svgRootGroup.selectAll('g.tkk');
     }
 
+    /**
+     * Private method; _fetchSvgFile.
+     *
+     * It fetches an SVG file from the given path via fetch method from D3 library.
+     *
+     * @param {string} path The path to the SVG file.
+     *
+     * @returns {Promise<Document>} A promise that resolves to the SVG file as Document.
+     */
     private _fetchSvgFile(path: string): Promise<Document> {
         return d3_fetch.svg(path);
     }
 
     /**
-     * Private method: _getElementDimensions.
+     * Private method: _getViewBox.
      *
-     * It returns the dimensions (clientWidth & clientHeight) of a given element.
+     * It reads out the viewBox attribute of a given SVGSVGElement and creates a ViewBox object.
      *
-     * @param {ElementRef} el The given element.
+     * @param {SVGSVGElement} svgXML The SVGSVGElement.
      *
-     * @returns { width: number; height: number } The element dimensions.
+     * @returns {ViewBox} The ViewBox object.
      */
-    private _getElementDimensions(el: ElementRef): { width: number; height: number } {
-        if (!el || !el.nativeElement) {
-            return null;
-        }
-        return { width: el.nativeElement.clientWidth, height: el.nativeElement.clientHeight };
-    }
-
     private _getViewBox(svgXML: SVGSVGElement): ViewBox {
         // Get the viewBox attributes from the SVG XML DOM tree
         const svgXMLViewBox: string = d3_selection.select(svgXML).attr('viewBox');
