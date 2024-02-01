0b4948646e7d67ef37cd335ee794c86affcf8bb7
musicEnfanthen <stefan.muennich@unibas.ch>
2024-02-01 17:31:56 +0100

    refactor(edition): move sheet nav components into separate module

--- src/app/views/edition-view/edition-outlets/edition-complex/edition-detail/edition-sheets/edition-accolade/edition-accolade.module.ts
+++ src/app/views/edition-view/edition-outlets/edition-complex/edition-detail/edition-sheets/edition-accolade/edition-accolade.module.ts
@@ -2,29 +2,24 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@awg-shared/shared.module';
 
 import { EditionTkaModule } from '../../edition-tka/edition-tka.module';
+import { EditionSvgSheetNavModule } from './edition-svg-sheet-nav/edition-svg-sheet-nav.module';
 
 import { EditionAccoladeComponent } from './edition-accolade.component';
 import { EditionSvgSheetFooterComponent } from './edition-svg-sheet-footer';
-import { EditionSvgSheetNavComponent } from './edition-svg-sheet-nav';
-import { EditionSvgSheetNavItemComponent } from './edition-svg-sheet-nav/edition-svg-sheet-nav-item/edition-svg-sheet-nav-item.component';
+
 import { EditionSvgSheetViewerComponent } from './edition-svg-sheet-viewer';
 
 /**
  * The EditionAccolade module.
  *
- * It embeds the edition accolade components as well as the {@link EditionSvgSheetFooterComponent},
- * {@link EditionSvgSheetNavComponent}, {@link EditionSvgSheetNavItemComponent},
- * {@link EditionSvgSheetViewerComponent}, {@link EditionTkaModule} and {@link SharedModule}.
+ * It embeds the edition accolade components
+ * as well as the {@link EditionSvgSheetFooterComponent},
+ * {@link EditionSvgSheetViewerComponent}, {@link EditionSvgSheetNavModule},
+ * {@link EditionTkaModule} and {@link SharedModule}.
  */
 @NgModule({
-    imports: [SharedModule, EditionTkaModule],
-    declarations: [
-        EditionAccoladeComponent,
-        EditionSvgSheetFooterComponent,
-        EditionSvgSheetNavComponent,
-        EditionSvgSheetNavItemComponent,
-        EditionSvgSheetViewerComponent,
-    ],
+    imports: [SharedModule, EditionSvgSheetNavModule, EditionTkaModule],
+    declarations: [EditionAccoladeComponent, EditionSvgSheetFooterComponent, EditionSvgSheetViewerComponent],
     exports: [EditionAccoladeComponent],
 })
 export class EditionAccoladeModule {}
