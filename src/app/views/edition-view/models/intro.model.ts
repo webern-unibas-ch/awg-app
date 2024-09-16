169be29fcb767f73ad0e98266490f715e6059dcb
musicEnfanthen <stefan.muennich@unibas.ch>
2024-09-16 18:37:21 +0200

    fix(assets): add full intro for AWG I/5

--- src/app/views/edition-view/models/intro.model.ts
+++ src/app/views/edition-view/models/intro.model.ts
@@ -1,4 +1,28 @@
 /**
+ * The IntroBlock class.
+ *
+ * It is used in the context of the edition view
+ * to store the data for a single intro block
+ * from a intro json file.
+ */
+export class IntroBlock {
+    /**
+     * The id of an intro block.
+     */
+    blockId: string;
+
+    /**
+     * The header of an intro block.
+     */
+    blockHeader: string;
+
+    /**
+     * The content array of an intro block.
+     */
+    blockContent: string[];
+}
+
+/**
  * The Intro class.
  *
  * It is used in the context of the edition view
@@ -14,7 +38,7 @@ export class Intro {
     /**
      * The content array of an intro.
      */
-    content: string[];
+    content: IntroBlock[];
 
     /**
      * The footnotes array of an intro.
