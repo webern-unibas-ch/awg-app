417d0a4d1380a97f9ef6db45656ab5e9a55a82f7
musicEnfanthen <stefan.muennich@unibas.ch>
2024-10-18 13:27:13 +0200

    fix(side-info): collapse section menus by default
    (cherry picked from commit 5f067573d2a550d545de6862cf828fa7b94a0d16)

--- src/app/side-info/edition-info/edition-info.component.spec.ts
+++ src/app/side-info/edition-info/edition-info.component.spec.ts
@@ -205,164 +205,230 @@ describe('EditionInfoComponent (DONE)', () => {
                 expectToBe(headerEl.textContent, expectedEditionInfoHeader);
             });
 
-            it('... should contain 3 div.accordion-items with header and open body in div.accordion', () => {
+            it('... should contain 3 div.accordion-items with header', () => {
                 // NgbAccordion debug element
                 const accordionDes = getAndExpectDebugElementByCss(compDe, 'div.accordion', 1, 1);
 
                 // Div.accordion-item
                 const itemDes = getAndExpectDebugElementByCss(accordionDes[0], 'div.accordion-item', 3, 3);
 
-                itemDes.forEach(item => {
+                itemDes.forEach(itemDe => {
                     // Header (div.accordion-header)
-                    getAndExpectDebugElementByCss(item, 'div.accordion-header', 1, 1);
+                    getAndExpectDebugElementByCss(itemDe, 'div.accordion-header', 1, 1);
+                });
+            });
 
+            it('... should only have open body for first div.accordion-item in div.accordion', () => {
+                // NgbAccordion debug element
+                const accordionDes = getAndExpectDebugElementByCss(compDe, 'div.accordion', 1, 1);
+
+                // Div.accordion-item
+                const itemDes = getAndExpectDebugElementByCss(accordionDes[0], 'div.accordion-item', 3, 3);
+
+                itemDes.forEach((itemDe, index) => {
                     // Item body
-                    const itemBodyDes = getAndExpectDebugElementByCss(itemDes[0], 'div.accordion-collapse', 1, 1);
+                    const itemBodyDes = getAndExpectDebugElementByCss(itemDe, 'div.accordion-collapse', 1, 1);
                     const itemBodyEl = itemBodyDes[0].nativeElement;
+
+                    if (index === 0) {
+                        expectToContain(itemBodyEl.classList, 'show');
+                    } else {
+                        expectToContain(itemBodyEl.classList, 'collapse');
+                    }
+                });
+            });
+
+            describe('... with open item bodies', () => {
+                beforeEach(() => {
+                    // Open second and third item
+                    // Div.accordion-item
+                    const itemDes = getAndExpectDebugElementByCss(compDe, 'div.accordion-item', 3, 3);
+
+                    // Header debug elements
+                    const itemHeader1Des = getAndExpectDebugElementByCss(itemDes[1], 'div.accordion-header', 1, 1);
+                    const itemHeader2Des = getAndExpectDebugElementByCss(itemDes[2], 'div.accordion-header', 1, 1);
+
+                    // Button debug elements
+                    const btn1Des = getAndExpectDebugElementByCss(itemHeader1Des[0], 'button.accordion-button', 1, 1);
+                    const btn2Des = getAndExpectDebugElementByCss(itemHeader2Des[0], 'button.accordion-button', 1, 1);
+
+                    // Button native elements to click on
+                    const btn1El = btn1Des[0].nativeElement;
+                    const btn2El = btn2Des[0].nativeElement;
+
+                    // Click header buttons to open
+                    click(btn1El as HTMLElement);
+                    click(btn2El as HTMLElement);
+                    detectChangesOnPush(fixture);
+                });
+
+                it('... should display item header buttons', () => {
+                    // Header debug elements
+                    const itemHeaderDes = getAndExpectDebugElementByCss(compDe, 'div.accordion-header', 3, 3);
+
+                    const expectedHeaders = [
+                        'Allgemein',
+                        `${expectedEditionRouteConstants.EDITION.short} ${expectedEditionRouteConstants.SERIES_1.short}/${expectedEditionRouteConstants.SECTION_5.short}`,
+                        `${expectedEditionRouteConstants.EDITION.short} ${expectedEditionRouteConstants.SERIES_2.short}/${expectedEditionRouteConstants.SECTION_2A.short}`,
+                    ];
+
+                    itemHeaderDes.forEach((itemHeaderDe, index) => {
+                        const btnDes = getAndExpectDebugElementByCss(itemHeaderDe, 'button.accordion-button', 1, 1);
+                        const btnEl = btnDes[0].nativeElement;
+
+                        // Check button contents
+                        expectToBe(btnEl.textContent.trim(), expectedHeaders[index]);
+                    });
+                });
+
+                it('... should toggle item body on click', () => {
+                    // Div.accordion-item
+                    const itemDes = getAndExpectDebugElementByCss(compDe, 'div.accordion-item', 3, 3);
+
+                    // Header debug elements
+                    const itemHeaderDes = getAndExpectDebugElementByCss(itemDes[0], 'div.accordion-header', 1, 1);
+
+                    // Button debug elements
+                    const btnDes = getAndExpectDebugElementByCss(itemHeaderDes[0], 'button.accordion-button', 1, 1);
+                    // Button native elements to click on
+                    const btnEl = btnDes[0].nativeElement;
+
+                    // Item body is open
+                    let itemBodyDes = getAndExpectDebugElementByCss(itemDes[0], 'div.accordion-collapse', 1, 1, 'open');
+                    let itemBodyEl = itemBodyDes[0].nativeElement;
+
+                    expectToContain(itemBodyEl.classList, 'show');
+
+                    // Click header button
+                    click(btnEl as HTMLElement);
+                    detectChangesOnPush(fixture);
+
+                    // Item body is collapsed
+                    itemBodyDes = getAndExpectDebugElementByCss(
+                        itemDes[0],
+                        'div.accordion-collapse',
+                        1,
+                        1,
+                        'collapsed'
+                    );
+                    itemBodyEl = itemBodyDes[0].nativeElement;
+
+                    expectToContain(itemBodyEl.classList, 'collapse');
+
+                    // Click header button
+                    click(btnEl as HTMLElement);
+                    detectChangesOnPush(fixture);
+
+                    // Item body is open again
+                    itemBodyDes = getAndExpectDebugElementByCss(itemDes[0], 'div.accordion-collapse', 1, 1, 'open');
+                    itemBodyEl = itemBodyDes[0].nativeElement;
 
                     expectToContain(itemBodyEl.classList, 'show');
                 });
-            });
 
-            it('... should display item header buttons', () => {
-                // Header debug elements
-                const itemHeaderDes = getAndExpectDebugElementByCss(compDe, 'div.accordion-header', 3, 3);
+                it('... should contain item body with 3 paragraphs in first item', () => {
+                    // Div.accordion-item
+                    const itemDes = getAndExpectDebugElementByCss(compDe, 'div.accordion-item', 3, 3);
 
-                const expectedHeaders = [
-                    'Allgemein',
-                    `${expectedEditionRouteConstants.EDITION.short} ${expectedEditionRouteConstants.SERIES_1.short}/${expectedEditionRouteConstants.SECTION_5.short}`,
-                    `${expectedEditionRouteConstants.EDITION.short} ${expectedEditionRouteConstants.SERIES_2.short}/${expectedEditionRouteConstants.SECTION_2A.short}`,
-                ];
+                    // Item body
+                    const itemBodyDes = getAndExpectDebugElementByCss(itemDes[0], 'div.accordion-body', 1, 1);
 
-                itemHeaderDes.forEach((itemHeaderDe, index) => {
-                    const btnDes = getAndExpectDebugElementByCss(itemHeaderDe, 'button.accordion-button', 1, 1);
-                    const btnEl = btnDes[0].nativeElement;
-
-                    // Check button contents
-                    expectToBe(btnEl.textContent.trim(), expectedHeaders[index]);
+                    // Paragraph
+                    getAndExpectDebugElementByCss(itemBodyDes[0], 'p', 3, 3);
                 });
-            });
 
-            it('... should toggle item body on click', () => {
-                // Div.accordion-item
-                const itemDes = getAndExpectDebugElementByCss(compDe, 'div.accordion-item', 3, 3);
+                it('... should contain item body with 5 paragraphs in second item', () => {
+                    // Div.accordion-item
+                    const itemDes = getAndExpectDebugElementByCss(compDe, 'div.accordion-item', 3, 3);
 
-                // Header debug elements
-                const itemHeaderDes = getAndExpectDebugElementByCss(itemDes[0], 'div.accordion-header', 1, 1);
+                    // Item body
+                    const itemBodyDes = getAndExpectDebugElementByCss(itemDes[1], 'div.accordion-body', 1, 1);
 
-                // Button debug elements
-                const btnDes = getAndExpectDebugElementByCss(itemHeaderDes[0], 'button.accordion-button', 1, 1);
-                // Button native elements to click on
-                const btnEl = btnDes[0].nativeElement;
+                    // Length of expected paragraphs (first items to slice index) plus intro
+                    const expectedLength = expectedEditionComplexes.slice(0, expectedSliceIndex).length + 1;
 
-                // Item body is open
-                let itemBodyDes = getAndExpectDebugElementByCss(itemDes[0], 'div.accordion-collapse', 1, 1, 'open');
-                let itemBodyEl = itemBodyDes[0].nativeElement;
-
-                expectToContain(itemBodyEl.classList, 'show');
-
-                // Click header button
-                click(btnEl as HTMLElement);
-                detectChangesOnPush(fixture);
-
-                // Item body is collapsed
-                itemBodyDes = getAndExpectDebugElementByCss(itemDes[0], 'div.accordion-collapse', 1, 1, 'collapsed');
-                itemBodyEl = itemBodyDes[0].nativeElement;
-
-                expectToContain(itemBodyEl.classList, 'collapse');
-
-                // Click header button
-                click(btnEl as HTMLElement);
-                detectChangesOnPush(fixture);
-
-                // Item body is open again
-                itemBodyDes = getAndExpectDebugElementByCss(itemDes[0], 'div.accordion-collapse', 1, 1, 'open');
-                itemBodyEl = itemBodyDes[0].nativeElement;
-
-                expectToContain(itemBodyEl.classList, 'show');
-            });
-
-            it('... should contain item body with 3 paragraphs in first item', () => {
-                // Div.accordion-item
-                const itemDes = getAndExpectDebugElementByCss(compDe, 'div.accordion-item', 3, 3);
-
-                // Item body
-                const itemBodyDes = getAndExpectDebugElementByCss(itemDes[0], 'div.accordion-body', 1, 1);
-
-                // Paragraph
-                getAndExpectDebugElementByCss(itemBodyDes[0], 'p', 3, 3);
-            });
-
-            it('... should contain item body with 5 paragraphs in second item', () => {
-                // Div.accordion-item
-                const itemDes = getAndExpectDebugElementByCss(compDe, 'div.accordion-item', 3, 3);
-
-                // Item body
-                const itemBodyDes = getAndExpectDebugElementByCss(itemDes[1], 'div.accordion-body', 1, 1);
-
-                // Length of expected paragraphs (first items to slice index) plus intro
-                const expectedLength = expectedEditionComplexes.slice(0, expectedSliceIndex).length + 1;
-
-                // Paragraphs
-                getAndExpectDebugElementByCss(itemBodyDes[0], 'p', expectedLength, expectedLength);
-            });
-
-            it('... should contain item body with 6 paragraphs in third item', () => {
-                // Div.accordion-item
-                const itemDes = getAndExpectDebugElementByCss(compDe, 'div.accordion-item', 3, 3);
-
-                // Item body
-                const itemBodyDes = getAndExpectDebugElementByCss(itemDes[2], 'div.accordion-body', 1, 1);
-
-                // Length of expected paragraphs (last items starting from slice index)
-                const expectedLength = expectedEditionComplexes.slice(expectedSliceIndex).length;
-
-                // Paragraphs
-                getAndExpectDebugElementByCss(itemBodyDes[0], 'p', expectedLength, expectedLength);
-            });
-
-            it('... should render links in edition info headers', () => {
-                const aDes = getAndExpectDebugElementByCss(
-                    compDe,
-                    'a.awg-edition-info-header-link',
-                    expectedOrderOfHeaders.length,
-                    expectedOrderOfHeaders.length
-                );
-
-                aDes.forEach((aDe, index) => {
-                    const aEl = aDe.nativeElement;
-
-                    expectToEqual(aEl.textContent, expectedOrderOfHeaders[index]);
+                    // Paragraphs
+                    getAndExpectDebugElementByCss(itemBodyDes[0], 'p', expectedLength, expectedLength);
                 });
-            });
 
-            it('... should render title of edition info headers', () => {
-                // Length of expected edition complexes plus one for the intro
-                const expectedLength = expectedEditionComplexes.length + 1;
+                it('... should contain item body with 6 paragraphs in third item', () => {
+                    // Div.accordion-item
+                    const itemDes = getAndExpectDebugElementByCss(compDe, 'div.accordion-item', 3, 3);
 
-                const titleDes = getAndExpectDebugElementByCss(
-                    compDe,
-                    'span.awg-edition-info-header-title',
-                    expectedLength,
-                    expectedLength
-                );
+                    // Item body
+                    const itemBodyDes = getAndExpectDebugElementByCss(itemDes[2], 'div.accordion-body', 1, 1);
 
-                titleDes.forEach((titleDe, index) => {
-                    const titleEl = titleDe.nativeElement;
+                    // Length of expected paragraphs (last items starting from slice index)
+                    const expectedLength = expectedEditionComplexes.slice(expectedSliceIndex).length;
 
-                    if (index === 0) {
-                        // The first title should be the intro title
-                        expectToBe(titleEl.innerText, EDITION_ROUTE_CONSTANTS.EDITION_INTRO.full);
-                    } else {
-                        // Subsequent titles should be the edition complex titles
-                        expectToBe(titleEl.innerHTML, expectedEditionComplexes[index - 1].complexId.full);
-                    }
+                    // Paragraphs
+                    getAndExpectDebugElementByCss(itemBodyDes[0], 'p', expectedLength, expectedLength);
+                });
+
+                it('... should render links in edition info headers', () => {
+                    const aDes = getAndExpectDebugElementByCss(
+                        compDe,
+                        'a.awg-edition-info-header-link',
+                        expectedOrderOfHeaders.length,
+                        expectedOrderOfHeaders.length
+                    );
+
+                    aDes.forEach((aDe, index) => {
+                        const aEl = aDe.nativeElement;
+
+                        expectToEqual(aEl.textContent, expectedOrderOfHeaders[index]);
+                    });
+                });
+
+                it('... should render title of edition info headers', () => {
+                    // Length of expected edition complexes plus one for the intro
+                    const expectedLength = expectedEditionComplexes.length + 1;
+
+                    const titleDes = getAndExpectDebugElementByCss(
+                        compDe,
+                        'span.awg-edition-info-header-title',
+                        expectedLength,
+                        expectedLength
+                    );
+
+                    titleDes.forEach((titleDe, index) => {
+                        const titleEl = titleDe.nativeElement;
+
+                        if (index === 0) {
+                            // The first title should be the intro title
+                            expectToBe(titleEl.innerText, EDITION_ROUTE_CONSTANTS.EDITION_INTRO.full);
+                        } else {
+                            // Subsequent titles should be the edition complex titles
+                            expectToBe(titleEl.innerHTML, expectedEditionComplexes[index - 1].complexId.full);
+                        }
+                    });
                 });
             });
         });
 
         describe('[routerLink]', () => {
             beforeEach(() => {
+                // Open second and third item
+                // Div.accordion-item
+                const itemDes = getAndExpectDebugElementByCss(compDe, 'div.accordion-item', 3, 3);
+
+                // Header debug elements
+                const itemHeader1Des = getAndExpectDebugElementByCss(itemDes[1], 'div.accordion-header', 1, 1);
+                const itemHeader2Des = getAndExpectDebugElementByCss(itemDes[2], 'div.accordion-header', 1, 1);
+
+                // Button debug elements
+                const btn1Des = getAndExpectDebugElementByCss(itemHeader1Des[0], 'button.accordion-button', 1, 1);
+                const btn2Des = getAndExpectDebugElementByCss(itemHeader2Des[0], 'button.accordion-button', 1, 1);
+
+                // Button native elements to click on
+                const btn1El = btn1Des[0].nativeElement;
+                const btn2El = btn2Des[0].nativeElement;
+
+                // Click header buttons to open
+                click(btn1El as HTMLElement);
+                click(btn2El as HTMLElement);
+                detectChangesOnPush(fixture);
+
                 // Find DebugElements with an attached RouterLinkStubDirective
                 linkDes = getAndExpectDebugElementByDirective(
                     compDe,
