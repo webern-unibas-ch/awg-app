358f5bc024e5cead88083b3532b7fc22d3cc7912
musicEnfanthen <stefan.muennich@unibas.ch>
2026-06-02 14:51:57 +0200

    test(edition): extend glyph tests

--- src/app/views/edition-view/services/edition-glyph.service.spec.ts
+++ src/app/views/edition-view/services/edition-glyph.service.spec.ts
@@ -23,18 +23,61 @@ describe('EditionGlyphService', () => {
     });
 
     describe('#getGlyph()', () => {
+        const expectGlyphMappings = (glyphMappings: Array<[string, string]>): void => {
+            glyphMappings.forEach(([alt, hex]) => {
+                expectToBe(editionGlyphService.getGlyph(alt), hex);
+            });
+        };
+
         it('... should have a method `getGlyph`', () => {
             expect(editionGlyphService.getGlyph).toBeDefined();
         });
 
-        it('... should return the correct hex value for a valid glyph alt value', () => {
-            expectToBe(editionGlyphService.getGlyph('[bb]'), '\uE264'); // DOUBLE_FLAT
-            expectToBe(editionGlyphService.getGlyph('[x]'), '\uE263'); // DOUBLE_SHARP
-            expectToBe(editionGlyphService.getGlyph('[b]'), '\uE260'); // FLAT
-            expectToBe(editionGlyphService.getGlyph('[#]'), '\uE262'); // SHARP
-            expectToBe(editionGlyphService.getGlyph('[a]'), '\uE261'); // NATURAL
-            expectToBe(editionGlyphService.getGlyph('[f]'), '\uE522'); // FORTE
-            expectToBe(editionGlyphService.getGlyph('[ped]'), '\uE650'); // PEDAL
+        it('... should return the correct hex value for accidentals', () => {
+            expectGlyphMappings([
+                ['[bb]', '\uE264'],
+                ['[x]', '\uE263'],
+                ['[b]', '\uE260'],
+                ['[#]', '\uE262'],
+                ['[a]', '\uE261'],
+            ]);
+        });
+
+        it('... should return the correct hex value for articulations', () => {
+            expectGlyphMappings([['[>]', '\uE4A0']]);
+        });
+
+        it('... should return the correct hex value for dynamics', () => {
+            expectGlyphMappings([
+                ['[f]', '\uE522'],
+                ['[ff]', '\uE52F'],
+                ['[fff]', '\uE530'],
+                ['[ffff]', '\uE531'],
+                ['[mf]', '\uE52D'],
+                ['[mp]', '\uE52C'],
+                ['[p]', '\uE520'],
+                ['[pp]', '\uE52B'],
+                ['[ppp]', '\uE52A'],
+                ['[pppp]', '\uE529'],
+                ['[sf]', '\uE536'],
+                ['[sfz]', '\uE539'],
+                ['[sp]', '\uE524\uE520'],
+            ]);
+        });
+
+        it('... should return the correct hex value for note values and pedal signs', () => {
+            expectGlyphMappings([
+                ['[Achtelnote]', '\uE1D7'],
+                ['[Halbe Note]', '\uE1D3'],
+                ['[Punktierte Halbe Note]', '\uE1D3 \uE1E7'],
+                ['[Viertelnote]', '\uE1D5'],
+                ['[Sechzehntelnote]', '\uE1D9'],
+                ['[Ganze Note]', '\uE1D2'],
+            ]);
+        });
+
+        it('... should return the correct hex value for playing techniques', () => {
+            expectGlyphMappings([['[ped]', '\uE650']]);
         });
 
         it('... should return an empty string for an invalid glyph alt value', () => {
