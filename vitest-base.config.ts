0a3e06027fb419ad1c01f9fabb0f59b40777710e
musicEnfanthen <stefan.muennich@unibas.ch>
2026-06-04 01:50:32 +0200

    build(app): update vitest config

--- vitest-base.config.ts
+++ vitest-base.config.ts
@@ -5,6 +5,5 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         globals: false, // (default: true)
-        isolate: true, // (default true)
     },
 });
