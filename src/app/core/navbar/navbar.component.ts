cea17a1024ef298132ce9b5e10afdf386ea14cd5
musicEnfanthen <stefan.muennich@unibas.ch>
2026-06-24 21:00:28 +0200

    fix(core): simplify navbar

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,17 +1,17 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
-import { isActive, Router, RouterLink } from '@angular/router';
+import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { isActive, Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { faEnvelope, faFileAlt, faHome, faNetworkWired, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope, faFileAlt, faHome, faNetworkWired } from '@fortawesome/free-solid-svg-icons';
 
 import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap/collapse';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap/dropdown';
 
+import { SharedModule } from '@awg-app/shared/shared.module';
 import { Logos } from '@awg-core/core-models';
 import { CoreService } from '@awg-core/services';
 import { EDITION_ROUTE_CONSTANTS } from '@awg-views/edition-view/edition-route-constants';
-import { EditionComplex } from '@awg-views/edition-view/models';
-import { EditionComplexesService } from '@awg-views/edition-view/services';
+import { EditionOutlineService } from '@awg-views/edition-view/services';
 import { LogoLinkComponent } from '../logo-link/logo-link.component';
 
 /**
@@ -25,7 +25,15 @@ import { LogoLinkComponent } from '../logo-link/logo-link.component';
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [FaIconComponent, LogoLinkComponent, NgbCollapse, NgbDropdownModule, RouterLink],
+    imports: [
+        FaIconComponent,
+        LogoLinkComponent,
+        NgbCollapse,
+        NgbDropdownModule,
+        RouterLink,
+        RouterLinkActive,
+        SharedModule,
+    ],
 })
 export class NavbarComponent {
     /**
@@ -43,11 +51,11 @@ export class NavbarComponent {
     private readonly _router = inject(Router);
 
     /**
-     * Public variable: isCollapsed.
+     * Public signal: isCollapsed.
      *
-     * It keeps the boolean value if the header menu is collapsed or not.
+     * It holds the boolean value if the header menu is collapsed or not.
      */
-    isCollapsed = true;
+    isCollapsed = signal(true);
 
     /**
      * Public readonly signal: logosData.
@@ -57,60 +65,37 @@ export class NavbarComponent {
     logosData = signal<Logos>(this._coreService.getLogos()).asReadonly();
 
     /**
-     * Public variable: navbarIcons.
+     * Readonly signal: sectionsData.
      *
-     * It keeps the fontawesome icons for the navbar.
+     * It keeps the array of displayed edition sections as a read-only signal.
      */
-    navbarIcons = {
-        contact: faEnvelope,
-        edition: faFileAlt,
-        home: faHome,
-        structure: faNetworkWired,
-        search: faSearch,
-    };
+    sectionsData = signal([
+        EditionOutlineService.getEditionSectionById('1', '5'),
+        EditionOutlineService.getEditionSectionById('2', '2a'),
+    ]).asReadonly();
 
     /**
-     * Public variable: navbarLabels.
+     * Computed signal: displayedSections.
      *
-     * It keeps the labels for the navbar.
+     * It computes the array of edition sections to be displayed in the navbar.
      */
-    navbarLabels = {
-        home: 'Home',
-        complexes: 'Auswahl Skizzenkomplexe',
-        contact: 'Kontakt',
-        edition: 'Edition',
-        general: 'Allgemein',
-        search: 'Datenbank-Suche',
-        structure: 'Strukturmodell',
-    };
+    displayedSections = computed(() => this.sectionsData().map(editionSection => {
+            const baseRoute = [
+                EDITION_ROUTE_CONSTANTS.EDITION.route,
+                EDITION_ROUTE_CONSTANTS.SERIES.route,
+                editionSection.seriesParent.route,
+                EDITION_ROUTE_CONSTANTS.SECTION.route,
+                editionSection.section.route,
+            ];
 
-    /**
-     * Readonly variable: DISPLAYED_EDITION_COMPLEXES.
-     *
-     * It keeps the array of displayed edition complexes.
-     */
-    readonly DISPLAYED_EDITION_COMPLEXES: EditionComplex[] = [
-        EditionComplexesService.getEditionComplexById('op3'),
-        EditionComplexesService.getEditionComplexById('op4'),
-        EditionComplexesService.getEditionComplexById('op12'),
-        EditionComplexesService.getEditionComplexById('op23'),
-        EditionComplexesService.getEditionComplexById('op25'),
-        EditionComplexesService.getEditionComplexById('m22'),
-        EditionComplexesService.getEditionComplexById('m30'),
-        EditionComplexesService.getEditionComplexById('m31'),
-        EditionComplexesService.getEditionComplexById('m34'),
-        EditionComplexesService.getEditionComplexById('m35_42'),
-        EditionComplexesService.getEditionComplexById('m37'),
-    ];
+            const shortTitle = `[${EDITION_ROUTE_CONSTANTS.EDITION.short} ${editionSection.seriesParent.short}/${editionSection.section.short}]`;
 
-    /**
-     * Getter variable: editionRouteConstants.
-     *
-     *  It returns the EDITION_ROUTE_CONSTANTS.
-     **/
-    get editionRouteConstants(): typeof EDITION_ROUTE_CONSTANTS {
-        return EDITION_ROUTE_CONSTANTS;
-    }
+            return {
+                baseRoute,
+                shortTitle,
+                fullTitle: editionSection.section.full,
+            };
+        }));
 
     /**
      * Public signal: isEditionRouteActive.
@@ -120,14 +105,89 @@ export class NavbarComponent {
     isEditionRouteActive = isActive('/edition', this._router);
 
     /**
+     * Public variable: navItems.
+     *
+     * It keeps the navigation items for the navbar.
+     */
+    navItems = {
+        home: {
+            id: 'home',
+            path: ['/home'],
+            label: 'Home',
+            icon: faHome,
+            spanClass: 'd-sm-none d-md-inline order-md-2',
+            iconClass: '',
+        },
+        edition: {
+            id: 'edition',
+            path: ['/edition'],
+            label: 'Edition',
+            icon: faFileAlt,
+            spanClass: 'd-sm-none d-md-inline',
+            iconClass: 'order-md-minus-1',
+        },
+        structure: {
+            id: 'structure',
+            path: ['/structure'],
+            label: 'Strukturmodell',
+            icon: faNetworkWired,
+            spanClass: 'd-sm-none d-md-inline order-md-2',
+            iconClass: '',
+        },
+        contact: {
+            id: 'contact',
+            path: ['/contact'],
+            label: 'Kontakt',
+            spanClass: 'd-sm-none d-lg-inline',
+            icon: faEnvelope,
+            iconClass: '',
+        },
+    } as const;
+
+    /**
+     * Readonly variable: GENERAL_EDITION_LINKS.
+     *
+     * It keeps the array of general edition links.
+     */
+    readonly GENERAL_EDITION_LINKS = [
+        {
+            path: [EDITION_ROUTE_CONSTANTS.EDITION.route, EDITION_ROUTE_CONSTANTS.SERIES.route],
+            label: EDITION_ROUTE_CONSTANTS.SERIES.full,
+        },
+        {
+            path: [EDITION_ROUTE_CONSTANTS.EDITION.route, EDITION_ROUTE_CONSTANTS.ROWTABLES.route],
+            label: EDITION_ROUTE_CONSTANTS.ROWTABLES.full,
+        },
+        {
+            path: [EDITION_ROUTE_CONSTANTS.EDITION.route, EDITION_ROUTE_CONSTANTS.PREFACE.route],
+            label: EDITION_ROUTE_CONSTANTS.PREFACE.full,
+        },
+    ];
+
+    /**
+     * Readonly variable: SECTION_EDITION_LINKS.
+     *
+     * It keeps the array of section edition links.
+     */
+    readonly SECTION_EDITION_LINKS = [
+        {
+            suffix: [EDITION_ROUTE_CONSTANTS.EDITION_INTRO.route],
+            label: 'Einleitung / Intro',
+        },
+        {
+            suffix: [], // Leeres Array für die nackte Basis-Route (Übersicht)
+            label: 'Übersicht',
+        },
+    ];
+
+    /**
      * Public method: toggleNav.
      *
-     * It toggles the isCollapsed flag.
+     * It toggles the isCollapsed signal.
      *
-     * @returns {boolean} Sets the isCollapsed flag.
+     * @returns {void} Updates the isCollapsed signal.
      */
-    toggleNav(): boolean {
-        this.isCollapsed = !this.isCollapsed;
-        return this.isCollapsed;
+    toggleNav(): void {
+        this.isCollapsed.update(collapsed => !collapsed);
     }
 }
