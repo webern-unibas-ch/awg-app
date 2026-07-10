859f234f228ae74f181dfb862c7ade861234c629
musicEnfanthen <stefan.muennich@unibas.ch>
2026-07-10 14:36:48 +0200

    refactor(shared): modernize LicenseComp

--- src/app/shared/license/license.component.spec.ts
+++ src/app/shared/license/license.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { beforeEach, describe, expect, it } from 'vitest';
 
-import { expectToBe, expectToContain, getAndExpectDebugElementByCss } from '@testing/expect-helper';
+import { expectToBe, expectToContain, expectToEqual, getAndExpectDebugElementByCss } from '@testing/expect-helper';
 
 import { LicenseComponent } from './license.component';
 
@@ -12,35 +12,34 @@ describe('LicenseComponent', () => {
     let fixture: ComponentFixture<LicenseComponent>;
     let compDe: DebugElement;
 
+    let expectedLicenseLink: string;
+    let expectedLicenseText: string;
     let expectedLicenseIcons: Array<{
         src: string;
         alt: string;
     }>;
-    let expectedLicenseLink: string;
-    let expectedLicenseRel: string;
-    let expectedLicenseText: string;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [LicenseComponent],
+            imports: [LicenseComponent],
         }).compileComponents();
     });
 
     beforeEach(() => {
+        // Test data
+        expectedLicenseLink = 'https://creativecommons.org/licenses/by-sa/4.0/';
+        expectedLicenseText =
+            'Creative Commons Namensnennung - Weitergabe unter gleichen Bedingungen 4.0 International Lizenz';
+        expectedLicenseIcons = [
+            { src: 'https://mirrors.creativecommons.org/presskit/icons/cc.svg', alt: 'CC' },
+            { src: 'https://mirrors.creativecommons.org/presskit/icons/by.svg', alt: 'BY' },
+            { src: 'https://mirrors.creativecommons.org/presskit/icons/sa.svg', alt: 'SA' },
+        ];
+
+        // Create component fixture
         fixture = TestBed.createComponent(LicenseComponent);
         component = fixture.componentInstance;
         compDe = fixture.debugElement;
-
-        // Test data
-        expectedLicenseIcons = [
-            { src: 'https://mirrors.creativecommons.org/presskit/icons/cc.svg', alt: 'License: CC icon' },
-            { src: 'https://mirrors.creativecommons.org/presskit/icons/by.svg', alt: 'License: CC Attribution icon' },
-            { src: 'https://mirrors.creativecommons.org/presskit/icons/sa.svg', alt: 'License: CC ShareAlike icon' },
-        ];
-        expectedLicenseLink = 'https://creativecommons.org/licenses/by-sa/4.0/';
-        expectedLicenseRel = 'license';
-        expectedLicenseText =
-            'Creative Commons Namensnennung - Weitergabe unter gleichen Bedingungen 4.0 International Lizenz';
     });
 
     it('... should create', () => {
@@ -48,52 +47,61 @@ describe('LicenseComponent', () => {
     });
 
     describe('BEFORE initial data binding', () => {
+        it('... should have `LICENSE_LINK`', () => {
+            expectToBe(component.LICENSE_LINK, expectedLicenseLink);
+        });
+
+        it('... should have `LICENSE_TEXT`', () => {
+            expectToBe(component.LICENSE_TEXT, expectedLicenseText);
+        });
+
+        it('... should have `LICENSE_ICONS`', () => {
+            expectToEqual(component.LICENSE_ICONS, expectedLicenseIcons);
+        });
+
         describe('VIEW', () => {
             it('... should contain one div.awg-license', () => {
                 getAndExpectDebugElementByCss(compDe, 'div.awg-license', 1, 1);
             });
 
-            it('... should contain one paragraph in div.awg-license', () => {
-                getAndExpectDebugElementByCss(compDe, 'p', 1, 1);
+            it('... should contain one paragraph with expected classes in div.awg-license', () => {
+                const divDes = getAndExpectDebugElementByCss(compDe, 'div.awg-license', 1, 1);
+                const pDes = getAndExpectDebugElementByCss(divDes[0], 'p', 1, 1);
+                const pEl: HTMLParagraphElement = pDes[0].nativeElement;
+
+                expectToContain(pEl.classList, 'small');
+                expectToContain(pEl.classList, 'text-center');
             });
 
-            it('... should contain one paragraph with small and text-center classes', () => {
-                getAndExpectDebugElementByCss(compDe, 'p.small.text-center', 1, 1);
-            });
-
-            it('... should contain one anchor with rel but no href yet', () => {
+            it('... should contain one anchor in paragraph', () => {
                 const pDes = getAndExpectDebugElementByCss(compDe, 'div.awg-license p', 1, 1);
-                const aDes = getAndExpectDebugElementByCss(pDes[0], 'a[rel="license"]', 1, 1);
+                getAndExpectDebugElementByCss(pDes[0], 'a.awg-license-link', 1, 1);
+            });
+
+            it('... should have rel but no href attribute on anchor yet', () => {
+                const aDes = getAndExpectDebugElementByCss(compDe, 'a.awg-license-link', 1, 1);
                 const aEl: HTMLAnchorElement = aDes[0].nativeElement;
 
                 expectToBe(aEl.href, '');
-                expectToBe(aEl.rel, expectedLicenseRel);
+                expectToBe(aEl.rel, 'license');
             });
 
-            it('... should contain license text in anchor', () => {
-                const pDes = getAndExpectDebugElementByCss(compDe, 'div.awg-license p', 1, 1);
-                const aDes = getAndExpectDebugElementByCss(pDes[0], 'a[rel="license"]', 1, 1);
+            it('... should contain no license text in anchor yet', () => {
+                const aDes = getAndExpectDebugElementByCss(compDe, 'a.awg-license-link', 1, 1);
                 const aEl: HTMLAnchorElement = aDes[0].nativeElement;
 
-                expectToContain(aEl.textContent, expectedLicenseText);
+                expectToBe(aEl.textContent, '');
             });
 
             it('... should contain license icon span in anchor', () => {
-                const pDes = getAndExpectDebugElementByCss(compDe, 'div.awg-license p', 1, 1);
-                const aDes = getAndExpectDebugElementByCss(pDes[0], 'a[rel="license"]', 1, 1);
+                const aDes = getAndExpectDebugElementByCss(compDe, 'a.awg-license-link', 1, 1);
 
                 getAndExpectDebugElementByCss(aDes[0], 'span.awg-license-icon', 1, 1);
             });
 
-            it('... should contain 3 img elements in icon span with no src yet', () => {
+            it('... should contain no img elements in icon pan yet', () => {
                 const spanDes = getAndExpectDebugElementByCss(compDe, 'span.awg-license-icon', 1, 1);
-                const imgDes = getAndExpectDebugElementByCss(spanDes[0], 'img', 3, 3);
-
-                imgDes.forEach((imgDe, index) => {
-                    const imgEl: HTMLImageElement = imgDe.nativeElement;
-                    expectToBe(imgEl.src, '');
-                    expectToBe(imgEl.alt, expectedLicenseIcons[index].alt);
-                });
+                getAndExpectDebugElementByCss(spanDes[0], 'img', 0, 0);
             });
         });
     });
@@ -105,22 +113,25 @@ describe('LicenseComponent', () => {
         });
 
         describe('VIEW', () => {
-            it('... should contain one anchor with correct href and rel attributes', async () => {
-                const pDes = getAndExpectDebugElementByCss(compDe, 'div.awg-license p', 1, 1);
-                const aDes = getAndExpectDebugElementByCss(pDes[0], 'a[rel="license"]', 1, 1);
+            it('... should contain license text in anchor', () => {
+                const aDes = getAndExpectDebugElementByCss(compDe, 'a.awg-license-link', 1, 1);
                 const aEl: HTMLAnchorElement = aDes[0].nativeElement;
-                fixture.detectChanges();
-                await new Promise(resolve => setTimeout(resolve, 0));
-                fixture.detectChanges();
 
-                console.log('aEl.href:', aEl.href);
-                console.log('aEl.rel:', aEl.rel);
-
-                expectToBe(aEl.href, expectedLicenseLink);
-                expectToBe(aEl.rel, expectedLicenseRel);
+                expectToContain(aEl.textContent, expectedLicenseText);
             });
 
-            it('... should contain 3 license icons in icon span', () => {
+            it('... should have expected href and rel attributes on anchor', async () => {
+                const aDes = getAndExpectDebugElementByCss(compDe, 'a.awg-license-link', 1, 1);
+                const aEl: HTMLAnchorElement = aDes[0].nativeElement;
+
+                expectToBe(aEl.href, expectedLicenseLink);
+                expectToContain(aEl.rel, 'license');
+                // Security attributes automatically added by ExternalLinkDirective
+                expectToContain(aEl.rel, 'noopener');
+                expectToContain(aEl.rel, 'noreferrer');
+            });
+
+            it('... should contain 3 image elements with license icons in icon span', () => {
                 const spanDes = getAndExpectDebugElementByCss(compDe, 'span.awg-license-icon', 1, 1);
                 const imgDes = getAndExpectDebugElementByCss(spanDes[0], 'img', 3, 3);
 
--- src/app/shared/license/license.component.ts
+++ src/app/shared/license/license.component.ts
@@ -1,17 +1,43 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { ExternalLinkDirective } from '../external-link/external-link.directive';
 
 /**
  * The License component.
  *
- * It contains the license template of the app
- * that is provided via the {@link SharedModule}.
+ * It contains the license template of the app.
  */
 @Component({
     selector: 'awg-license',
     templateUrl: './license.component.html',
     styleUrls: ['./license.component.scss'],
-    standalone: false,
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [ExternalLinkDirective],
 })
 export class LicenseComponent {
-    // Intentionally left empty
+    /**
+     * Readonly variable: LICENSE_LINK
+     *
+     * It contains the link to the license.
+     */
+    readonly LICENSE_LINK = 'https://creativecommons.org/licenses/by-sa/4.0/';
+
+    /**
+     * Readonly variable: LICENSE_TEXT
+     *
+     * It contains the short text of the license.
+     */
+    readonly LICENSE_TEXT =
+        'Creative Commons Namensnennung - Weitergabe unter gleichen Bedingungen 4.0 International Lizenz';
+
+    /**
+     * Readonly variable: LICENSE_ICONS
+     *
+     * It contains the icons of the license.
+     */
+    readonly LICENSE_ICONS = [
+        { src: 'https://mirrors.creativecommons.org/presskit/icons/cc.svg', alt: 'CC' },
+        { src: 'https://mirrors.creativecommons.org/presskit/icons/by.svg', alt: 'BY' },
+        { src: 'https://mirrors.creativecommons.org/presskit/icons/sa.svg', alt: 'SA' },
+    ];
 }
